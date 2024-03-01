a0a83ab81068380eda18173e8e15f5ff5ab874b4
injeolmeeeeeee <86404260+injeolmeeeeeee@users.noreply.github.com>
2024-03-01 15:59:49 +0800

    commit

--- ecommerce/src/main/java/vttp/batch4/csf/ecommerce/controllers/ProductController.java
+++ ecommerce/src/main/java/vttp/batch4/csf/ecommerce/controllers/ProductController.java
@@ -3,12 +3,10 @@ package vttp.batch4.csf.ecommerce.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import jakarta.json.Json;
@@ -21,7 +19,6 @@ import vttp.batch4.csf.ecommerce.services.ProductService;
 // If this class is changed, any assessment task relying on this class will
 // not be marked
 @Controller
-@RequestMapping(path="/api", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ProductController {
 
   @Autowired
